8cb922395b8a199e1aad4c8d6e4b08b2010f156f
qweArteom <kurylenko2artem@gmail.com>
2024-11-08 22:29:59 +0200

    add_travel_tour

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app.secret_key = 'supersecretkey'
 
 
 tours = []
+travel_tours = []
 
 
 @app.route('/booking', methods=['GET', 'POST'])
@@ -13,10 +14,9 @@ def booking():
         name = request.form['name']
         email = request.form['email']
         tour = request.form['tour']
-    
         flash(f'Дякуємо за бронювання, {name}! Ви забронювали тур "{tour}".')
         return redirect(url_for('booking'))
-    return render_template('booking.html', tours=tours)
+    return render_template('booking.html', tours=tours + travel_tours)
 
 
 @app.route('/add_tour', methods=['GET', 'POST'])
@@ -25,11 +25,30 @@ def add_tour():
         tour_name = request.form['tour_name']
         description = request.form['description']
         price = request.form['price']
-      
         tours.append({'name': tour_name, 'description': description, 'price': price})
         flash(f'Тур "{tour_name}" успішно доданий!')
         return redirect(url_for('add_tour'))
     return render_template('add_tour.html')
+
+
+@app.route('/add_travel_tour', methods=['GET', 'POST'])
+def add_travel_tour():
+    if request.method == 'POST':
+        tour_name = request.form['tour_name']
+        destination = request.form['destination']
+        description = request.form['description']
+        duration = request.form['duration']
+        price = request.form['price']
+        travel_tours.append({
+            'name': tour_name,
+            'destination': destination,
+            'description': description,
+            'duration': duration,
+            'price': price
+        })
+        flash(f'Подорожній тур "{tour_name}" успішно доданий!')
+        return redirect(url_for('add_travel_tour'))
+    return render_template('add_travel_tour.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
